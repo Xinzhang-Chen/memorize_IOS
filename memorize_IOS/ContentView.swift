61b1f3d822f2fa46aea0ffb45c40e89274005690
Johnson-GitHub-New <64173435+Johnson-GitHub-New@users.noreply.github.com>
2023-01-03 13:05:06 +1100

    Initial commit

--- memorize_IOS/ContentView.swift
+++ memorize_IOS/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return ZStack(alignment: .center, content: {
+            RoundedRectangle(cornerRadius: 18.0)
+                .stroke(lineWidth: 3)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.pink/*@END_MENU_TOKEN@*/)
+            Text("Hello, world!")
+                .foregroundColor(Color.orange)
+        })
+            .padding(20)
+        
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
     }
 }
