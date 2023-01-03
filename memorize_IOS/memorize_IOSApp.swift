d26b5abd48c64c28fcc9ccaada5ab88cb64e96d9
Johnson-GitHub-New <64173435+Johnson-GitHub-New@users.noreply.github.com>
2023-01-03 11:39:45 +1100

    Initial Commit

---
+++ memorize_IOS/memorize_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorize_IOSApp.swift
+//  memorize_IOS
+//
+//  Created by Johnson Chen on 3/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct memorize_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
